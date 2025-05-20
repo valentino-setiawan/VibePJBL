869d0c541dbb468d69550b96ac16fbbfcb8cc2ba
valentino-setiawan <gunturwrh@gmail.com>
2025-05-20 08:53:23 +0700

    V0.1, working prototype

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ 
+  apiKey: ""
+ });
+
+const inputChat = document.querySelector("#chat");
+const outputChat = document.querySelector("#yap");
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
 
   return (
     <>
@@ -18,12 +28,27 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <input type='text' id='chat'></input>
+        <button onClick={() => {    
+          async function main() {
+          const question = inputChat.value;
+
+          console.log(question);
+          const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash",
+            contents: `${question}, answer in less than 30 words` ,
+          });
+          console.log(response.text);
+          const answer = response.text;
+          outputChat.innerHTML = answer;
+          }
+
+          main();
+          }}
+        >
+          ask about
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <p id='yap'></p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
